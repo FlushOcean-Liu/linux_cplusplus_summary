15b81afc26558a4898f598d4135b86be6877a978
LIU_centos <1509444199@qq.com>
2016-04-15 09:27:28 +0800

    fix quick sort

--- sort.c
+++ sort.c
@@ -19,9 +19,11 @@ static int partition(int a[], int low, int high)
 	int pivot = a[low];
 	while(low < high)
 	{
-		while( (low < high) && (a[high] > pivot)) high--;
+		while( (low < high) && (a[high] > pivot)) 
+			high--;
 		a[low] = a[high];
-		while( (low < high) && (a[high] <= pivot)) low++;
+		while( (low < high) && (a[low] <= pivot)) 
+			low++;
 		a[high] = a[low]; 
 	}
 	a[low] = pivot;
