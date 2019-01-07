f0ca76a959a4327c0407a0517edf7ebf59b671d6
FlushOcean.Liu <1509444199@qq.com>
2019-01-07 19:51:41 +0800

    Create sort.h

---
+++ sort_algorithm/sort.h
@@ -0,0 +1,16 @@
+#ifndef __SORT_H__
+#define __SORT_H__
+
+
+void quick_sort(int a[], int low, int high);
+
+void print(int a[],int n);
+
+void insert_sort(int a[], int len);
+
+void buble_sort(int a[], int len);
+
+void shell_sort(int a[], int len);
+
+#endif
+
