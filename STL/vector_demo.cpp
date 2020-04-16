102aabd17dcba56ad50e94d0d793a50efba877a3
root <root@localhost.localdomain>
2020-04-16 23:46:55 +0800

    刘光海:更新vector

--- STL/vector_demo.cpp
+++ STL/vector_demo.cpp
@@ -1,3 +1,14 @@
+/*
+ * vector 模板使用
+ *成员函数：
+ *push_back() 添加元素,pop_back()删除元素 
+ *size()返回容器大小
+ *capacity()返回当前容器能容纳的元素
+ *empty()判断容器是否为空
+ *clear()清零 
+
+*/ 
+
 #include <iostream>
 #include <vector>
 
@@ -6,21 +17,25 @@ using namespace std;
 int main()
 {
 	vector<int> a;
-	a.reserve(4);
-	cout<<"capacity is "<<a.capacity()<<endl;
-	a = {1,2,5,0,-2};
-	cout<<"capacity is "<< a.capacity()<<endl;
+	a.reserve(4);     /* reserve 预留空间，新元素没有构造，不能使用[]访问*/ 
+	cout<<"[1]capacity is "<<a.capacity()<<" size is "<<a.size()<<endl;
+	a = {1,2,5,0,-2}; /* c++11 赋值方式 */ 
+	cout<<"[2]capacity is "<<a.capacity()<<" size is "<<a.size()<<endl;
+	a.resize(10);    /* 分配100空间，不占用原有空*/
+	cout<<"[3]capacity is "<<a.capacity()<<" size is "<<a.size()<<endl;
 	
+	/* 使用iterator遍历容器元素 */ 
 	for(vector<int>::iterator it=a.begin();it!=a.end();it++){
 		cout<<*it<<" ";
 	}
 	cout<<endl;
-	for(auto ch:a) {
-		
+	
+	/* 使用auto遍历容器元素 */ 
+	for(auto ch:a) {	
 		cout<<ch<<" ";
 	}
 	cout<<endl;
-	cout<<"front is "<<a.front()<<" back is "<<a.back()<<" ÏÂ±ê2µÄÔªËØ"<<a.at(2)<<endl;
+	cout<<"front is "<<a.front()<<" back is "<<a.back()<<" 下标2的元素"<<a.at(2)<<endl;
 	
 	a.pop_back();
 	a.push_back(19);
@@ -32,3 +47,4 @@ int main()
 	
 	return 0;
 }
+
